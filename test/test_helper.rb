045534b1f18561cef50c523976eeddad87ab0f3c
Gareth Townsend <gareth.townsend@me.com>
2014-03-03 06:47:00 +1100

    Require minitest and alias context to describe.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,12 @@
 require 'jfit'
+require 'minitest'
 require 'minitest/autorun'
 require 'minitest/spec'
+
+module MiniTest
+  class Spec
+    class << self
+      alias_method :context, :describe
+    end
+  end
+end
