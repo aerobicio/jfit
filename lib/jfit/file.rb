bdae90343820b6009d142094c6a0618a409ab995
Gareth Townsend <gareth.townsend@me.com>
2014-03-03 21:32:36 +1100

    Port the DecodeExample to Jfit::File. Can now read file_id messages.

--- lib/jfit/file.rb
+++ lib/jfit/file.rb
@@ -1,11 +1,57 @@
+require 'java'
+require 'vendor/fit.jar'
+
 module Jfit
   class File
+    attr_accessor :valid, :file_id
+
     def self.read(io)
       new.read(io)
     end
 
     def read(io)
+      @valid = Java::ComGarminFit::Decode.check_integrity(io.to_inputstream)
+
+      if @valid
+        io.rewind
+
+        decode = Java::ComGarminFit::Decode.new
+        message_broadcaster = Java::ComGarminFit::MesgBroadcaster.new(decode)
+        @file_id = FileID.new
+
+        message_broadcaster.add_listener(@file_id)
+        message_broadcaster.run(io.to_inputstream)
+      end
+
+      io.close
+
       self
     end
+  end
+end
+
+class FileID
+  include Java::ComGarminFit::FileIdMesgListener
+
+  attr_accessor :type, :manufacturer, :product, :serial_number
+
+  java_signature 'void onMesg(Java::ComGarminFit::FileIdMesg)'
+  def onMesg(message)
+    if message.get_type != nil && message.getType != Java::ComGarminFit::File::INVALID
+      @type = message.get_type.get_value
+    end
+
+    if message.get_manufacturer != nil && message.get_manufacturer != Java::ComGarminFit::Manufacturer::INVALID
+      @manufacturer = message.get_manufacturer
+    end
+
+    if message.get_product != nil && message.get_product != Java::ComGarminFit::Fit::UINT16_INVALID
+      @product = message.get_product
+    end
+
+    if message.get_serial_number != nil && message.get_serial_number != Java::ComGarminFit::Fit::UINT32Z_INVALID
+      @serial_number = message.get_serial_number
+    end
+
   end
 end
--- test/integration/forerunner/decode_620_test.rb
+++ test/integration/forerunner/decode_620_test.rb
@@ -10,11 +10,34 @@ describe Jfit::File do
 
       it { subject.must_be_instance_of(Jfit::File) }
 
+      it "must be a valid file" do
+        subject.valid.must_equal(true)
+      end
+
       # File ID:
       #  Type: 4
       #  Manufacturer: 1
       #  Product: 1623
       #  Serial Number: 3877468399
+      context "#file_id" do
+        subject { Jfit::File.read(io).file_id }
+
+        it "must have a type of 4" do
+          subject.type.must_equal(4)
+        end
+
+        it "must have a manufacturer of 1" do
+          subject.manufacturer.must_equal(1)
+        end
+
+        it "must have a product of 1623" do
+          subject.product.must_equal(1623)
+        end
+
+        it "must have a serial number of 3877468399" do
+          subject.serial_number.must_equal(3877468399)
+        end
+      end
     end
   end
 end
